ca512aeeada95b3a02530371cec341edbb142280
52lions06 <mckelvey.william@yahoo.com>
2017-12-05 14:44:32 -0500

    added state to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import Questions from './components/Questions';
 import './App.css';
 
 class App extends Component {
+   constructor(props) {
+    super(props);
+    this.state = {
+     counter: 0,
+     questionId: 1,
+     question: '',
+     answerOptions: [],
+     answer: '',
+     answersCount: {
+       nintendo: 0,
+       microsoft: 0,
+       sony: 0
+     },
+     result: ''
+    };
+  }
+  
   render() {
     return (
       <div className="App">
