1b5ff9c3673119bc34b036e30f508d378a2fc116
52lions06 <mckelvey.william@yahoo.com>
2017-12-05 14:03:21 -0500

    created question stateless component

---
+++ src/components/Questions.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+  function Questions(props) {
+    return (
+      <h2 className="questions">{props.content}</h2>
+    );
+  }
+
+  Questions.propTypes = {
+    content: React.PropTypes.string.isRequired
+  };
+
+  export default Questions;
+
+  //stateless component 
+  //container component -- creates the questions
